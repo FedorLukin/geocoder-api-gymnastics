98e5f93113ce4c3489f7328ad2fefca317454796
Fedor <egorhorishev@gmail.com>
2025-04-05 15:15:19 +0700

    init

---
+++ search.py
@@ -0,0 +1,12 @@
+import sys
+import requests
+from PIL import Image
+from io import BytesIO
+
+
+toponym_to_find = " ".join(sys.argv[1:])
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+geocoder_params = {
+    "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
+    "geocode": toponym_to_find,
+    "format": "json"}

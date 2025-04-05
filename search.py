7950b5ae2f3149545a33aa23de3a1f542ac75d5f
Fedor Lukin <Fedor47365@yandex.ru>
2025-04-06 00:05:49 +0700

    final answer

--- search.py
+++ search.py
@@ -2,11 +2,22 @@ import sys
 import requests
 from PIL import Image
 from io import BytesIO
-
+from get_object import get_object_size_and_cords
 
 toponym_to_find = " ".join(sys.argv[1:])
-geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-geocoder_params = {
-    "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
-    "geocode": toponym_to_find,
-    "format": "json"}
+object = get_object_size_and_cords(toponym_to_find)
+
+apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+
+map_params = {
+    "ll": object.point,
+    "spn": object.spn,
+    "apikey": apikey,
+    "pt": f'{object.point},round'
+}
+
+map_api_server = "https://static-maps.yandex.ru/v1"
+response = requests.get(map_api_server, params=map_params)
+im = BytesIO(response.content)
+opened_image = Image.open(im)
+opened_image.show()
